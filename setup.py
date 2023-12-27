f56d1b95a4915136778b57d49c2e5b29cea96ab0
manish72 <manishguru72@gmail.com>
2023-12-27 12:38:56 +0530

    file added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "Medical Chatbot",
+    version = "0.0.0",
+    author = "Guru Manish",
+    author_email = "manishvankadhara72@gmail.com",
+    packages = find_packages(),
+    install_requires = []
+)
